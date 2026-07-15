56b60a83e5058fd04530ea37b48cf77f89e99ece
sumishaw <55247599+sumishaw@users.noreply.github.com>
2026-07-15 18:36:18 +0530

    Add material-icons-extended dependency

--- VideoDownloaderApp/app/build.gradle.kts
+++ VideoDownloaderApp/app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
 
     // Networking
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
